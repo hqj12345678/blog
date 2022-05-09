ea0a79182339566b164fa83514af0f2493f2b5d4
沉默小子 <2557185023@qq.com>
2022-05-09 12:01:21 +0800

    更新redis配置

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -134,7 +134,7 @@ CACHES = {
         }
     },
     'session':{
-        'BACKEND':'django_redis.RedisCache',
+        'BACKEND':'django_redis.cache.RedisCache',
         'LOCATION':'redis://127.0.0.1:6379/1',
         'OPTIONS':{
             'CLIENT_CLASS':'django_redis.client.DefaultClient',
---
+++ blog/blog/views.py
@@ -0,0 +1,2 @@
+from  django_redis.cache import RedisCache
+from django.core.cache import get_cache
